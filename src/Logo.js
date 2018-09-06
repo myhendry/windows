434e78abfe126379d4709d2a96db26abcbd90bd4
Hendry <myhendry@gmail.com>
2018-09-06 09:18:43 +0800

    Initial Commit

---
+++ src/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Logo extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Hey</Text>
+            </View>
+        )
+    }
+}
+
+export default Logo
